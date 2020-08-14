d09091f3ef798be99b4879c00f395270ab382ec8
Jacek Gajek <jacek.s.gajek@gmail.com>
2020-08-14 13:59:31 +0200

    add timestamp

--- src/main/kotlin/clebre/notes/NoteController.kt
+++ src/main/kotlin/clebre/notes/NoteController.kt
@@ -1,6 +1,7 @@
 package clebre.notes
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +14,7 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 class NoteController(val crud: NoteCrudService) {
 
