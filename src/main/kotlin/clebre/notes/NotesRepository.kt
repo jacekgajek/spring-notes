8f07b9fe650b4b8a011a07b5be0cd452399ee1d1
Jacek Gajek <jacek.s.gajek@gmail.com>
2020-08-14 13:59:24 +0200

    switch to mongodb

--- src/main/kotlin/clebre/notes/NotesRepository.kt
+++ src/main/kotlin/clebre/notes/NotesRepository.kt
@@ -1,15 +1,15 @@
 package clebre.notes
 
-import org.springframework.data.cassandra.core.mapping.PrimaryKey
-import org.springframework.data.cassandra.core.mapping.Table
-import org.springframework.data.cassandra.repository.CassandraRepository
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.repository.MongoRepository
+import java.time.LocalDateTime
 import java.util.*
 
 
-@Table
-data class Note(@PrimaryKey val id: UUID = UUID.randomUUID(),
+data class Note(@Id val id: UUID = UUID.randomUUID(),
                 val title: String,
                 val description: String,
+                val timestamp: LocalDateTime = LocalDateTime.now(),
                 val starred: Boolean = false)
 
-interface NotesRepository : CassandraRepository<Note, UUID>
+interface NotesRepository : MongoRepository<Note, UUID>
